aa4d73c5f70b43a68810622acfaefaf7ff1178d8
M.S <www.matinsnati0901@gmail.com>
2025-04-17 13:03:01 +0330

    تعغیر کامل ساختار لودینگ

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -3,12 +3,17 @@ import '../../styles/Loading/Loading.css';
 
 export const Loading = () => {
     return (
-        <div className="loading-wrapper">
-            <div className="neon-loader">
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
+        <div className="loading-scene">
+            <div className="planet-container">
+                <div className="planet"></div>
+                <div className="ring ring1"></div>
+                <div className="ring ring2"></div>
+                <div className="pulse-glow"></div>
+            </div>
+            <div className="stars">
+                {Array.from({ length: 40 }).map((_, i) => (
+                    <div key={i} className="star" />
+                ))}
             </div>
         </div>
     );
